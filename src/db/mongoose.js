d3434ea49fde83e3cdb92c6367bedc7a184df287
David405 <davidasamonye@gmail.com>
2021-01-21 13:05:23 +0100

    added dotenv config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // connecting to the database
 mongoose.connect(process.env.MONGODB_URL, {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require ('express')
 require('./db/mongoose')
 const routes = require('./routes')
 const cors = require('cors');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express()
 
