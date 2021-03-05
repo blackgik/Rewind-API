c22e9227245f464e9cc9c76da057161a3cdd33e7
echendu Chinedum <echendu.chinedum@gmail.com>
2021-03-05 12:15:16 +0100

    fix: console logging my error to check

--- src/controllers/userRoleAuth.js
+++ src/controllers/userRoleAuth.js
@@ -57,7 +57,7 @@ const userRegistration = async (userDet, res, role) => {
             success: true
         })
     }catch(err) {
-        // console.log(err)
+        console.log(err)
         return res.status(500).json({
             message: 'unable to register the user'
         })
